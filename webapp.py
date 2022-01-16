6f4391eb8d45d1db952c186afafea2d2eb8f82ae
rydr0 <56206508+rydr0@users.noreply.github.com>
2022-01-16 12:11:40 -0800

    added topshot categories at end

--- webapp.py
+++ webapp.py
@@ -9,10 +9,12 @@ df = get_daily_player_data()
 df.style.hide_index()
 
 df['moment'] = df['Set']+'-'+df['Tier']+'-'+df['Series']+'-'+df['Play']
+
 fixed_categories = ['jerseyNum']
 game_detail_categories = ['team','opp', 'minutes','score','game_clock']
 topshot_categories = ['moment','Circulation Count','Low Ask']
 stat_categories = ['points','reboundsTotal','assists','steals','blocks']
+
 sort_by = ['points', 'minutes']
 asc_list = [False, False]
 
@@ -24,6 +26,6 @@ for cat in fixed_categories:
 options = st.sidebar.multiselect(
      'Which Stat Categories are you interested in?',columns,stat_categories)
 
-categories = fixed_categories+options+game_detail_categories+stat_categories
+categories = fixed_categories+options+game_detail_categories+topshot_categories
 
 st.dataframe(df.sort_values(sort_by, ascending=asc_list)[categories], height=1200)
