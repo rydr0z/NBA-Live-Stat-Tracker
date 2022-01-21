76cd09715eae60ad349cbf11f119fc9eb2e59fec
rydr0 <56206508+rydr0@users.noreply.github.com>
2022-01-21 13:35:08 -0800

    increasing sleep again

--- nba_boxscore_fetcher.py
+++ nba_boxscore_fetcher.py
@@ -14,7 +14,7 @@ from nba_api.stats.endpoints import teamplayerdashboard
 @st.cache(allow_output_mutation=True)
 def get_team_stats(team_id):
     team_player_dash = teamplayerdashboard.TeamPlayerDashboard(team_id, timeout=120)
-    time.sleep(2)
+    time.sleep(5)
     dict = team_player_dash.get_dict()
     data = dict["resultSets"][1]["rowSet"]
     columns = dict["resultSets"][1]["headers"]
