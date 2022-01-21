279e74a139d0baae8d6d8bc06f2a10d298ba2265
rydr0 <56206508+rydr0@users.noreply.github.com>
2022-01-21 13:41:02 -0800

    updated headers in request

--- nba_boxscore_fetcher.py
+++ nba_boxscore_fetcher.py
@@ -11,16 +11,21 @@ from nba_api.live.nba.endpoints import boxscore
 from nba_api.stats.endpoints import teamplayerdashboard
 
 headers = {
-    "Connection": "keep-alive",
     "Accept": "application/json, text/plain, */*",
-    "x-nba-stats-token": "true",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
-    "x-nba-stats-origin": "stats",
-    "Sec-Fetch-Site": "same-origin",
-    "Sec-Fetch-Mode": "cors",
-    "Referer": "https://stats.nba.com/",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "Host": "stats.nba.com",
+    "Origin": "https://www.nba.com",
+    "Referer": "https://www.nba.com/",
+    "sec-ch-ua": '"Google Chrome";v="87", ""Not;A\\Brand";v="99", "Chromium";v="87"',
+    "sec-ch-ua-mobile": "?1",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36",
+    "x-nba-stats-origin": "stats",
+    "x-nba-stats-token": "true",
 }
 
 
