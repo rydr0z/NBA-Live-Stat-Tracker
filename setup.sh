8f2a5bb5812f894466ae0b33f68f79d96cc2db79
rydr0 <56206508+rydr0@users.noreply.github.com>
2022-01-27 13:51:00 -0800

    reqs for heroku hosting

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"rdrozdz@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
