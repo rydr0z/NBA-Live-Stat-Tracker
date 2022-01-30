90bcf1cda6a97c6a818dfdfd8b694a8c0e7be1a3
rydr0 <56206508+rydr0@users.noreply.github.com>
2022-01-29 19:45:02 -0800

    updated headers

--- data_fetchers/season/constants.py
+++ data_fetchers/season/constants.py
@@ -59,22 +59,15 @@ class SeasonParameters:
     ]
 
     HEADERS = {
-        "Accept": "application/json, text/plain, */*",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Connection": "keep-alive",
-        "Host": "stats.nba.com",
-        "Origin": 'https://www.nba.com',
-        "Referer": 'https://www.nba.com/',
-        "sec-ch-ua": '"Google Chrome";v="87", ""Not;A\\Brand";v="99", "Chromium";v="87"',
-        "sec-ch-ua-mobile": "?1",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-site",
-        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/87.0.4280.141 Mobile Safari/537.36",
-        "x-nba-stats-origin": "stats",
-        "x-nba-stats-token": "true",
+        'Host': 'stats.nba.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://stats.nba.com/',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'x-nba-stats-origin': 'stats',
+        'x-nba-stats-token': 'true'
     }
     SLEEP_INTERVAL = 1
     GAME_DATE_OBJECT = GameDate()
