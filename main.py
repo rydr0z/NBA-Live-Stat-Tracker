dd85df7fdc90ab204e6e5fbcb62abee0a796eacf
rydr0 <56206508+rydr0@users.noreply.github.com>
2022-02-25 14:06:45 -0800

    Updated post all star break

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import streamlit as st
-
 from webapp.webapp import run_webapp
 
 
 def main():
-    if True:
-        st.image("NBA Stat Logo.png")
-        st.title("NBA All Star Game")
-        st.write("Check back when regular season resumes")
-    else:
-        run_webapp()
+    run_webapp()
     print("Starting NBA Stats WebApp")
 
 
